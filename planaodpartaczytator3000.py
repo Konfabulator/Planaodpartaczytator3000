7e07bd754b017c732ac9001b5c2e32bf849c27a2
Konfabulator <grzesszymanek29@gmail.com>
2022-06-16 17:08:38 +0200

    planaodpartaczytator3000

--- planaodpartaczytator3000.py
+++ planaodpartaczytator3000.py
@@ -1,3 +1,4 @@
+# planaodpartaczytator3000
 import os
 from tabulate       import tabulate
 from decoder        import decode_subj_file
@@ -7,8 +8,8 @@ directory = os.path.dirname(os.path.realpath(__file__)) + '//'
 tabelka = lambda df:tabulate(df,Kolumny,tablefmt='presto')
 
 file_to_read = input("Wprowadź nazwę pliku z godzinami")                        #"Z2022_23.txt"
-file_to_write = "Plany_"+file_to_read 
-schedules = open(directory + '\\'  + file_to_write, "w+", encoding="utf-8")     # plik wyjściowy z planami zajęć
+file_to_write = "Plany_" + file_to_read                                         # plik wyjściowy z planami zajęć
+schedules = open(directory + '\\'  + file_to_write, "w+", encoding="utf-8")     # gotowe plany zajęć
 
 Kolumny = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
 Wiersze = ['8','9','10','11','12','13','14','15','16','17','18']
